f5e380383068c1b767bb66088c32b3fcc69a98b5
Samuel Sjoberg <samuel.sjoberg@extendaretail.com>
2020-02-19 21:04:44 +0100

    feat: add imperative tense commitlint rule

--- pre-commitlint/commitlint.config.js
+++ pre-commitlint/commitlint.config.js
@@ -2,7 +2,11 @@ module.exports = {
   extends: [
     '@commitlint/config-conventional',
   ],
+  plugins: [
+    'tense',
+  ],
   rules: {
-    "scope-case": [0, "always", "lower-case"] // Disabled
+    'scope-case': [0, 'always', 'lower-case'],
+    'imperative-tense': [2, 'always'],
   }
 };
