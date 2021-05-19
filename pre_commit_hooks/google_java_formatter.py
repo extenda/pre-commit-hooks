a2cff18875b27515917ea07d95996e12e6b3fc3c
misil <mikael.silfver@gmail.com>
2021-05-19 16:19:27 +0200

    fix: Split arguments and warn if java < 11

--- pre_commit_hooks/google_java_formatter.py
+++ pre_commit_hooks/google_java_formatter.py
@@ -31,22 +31,27 @@ def get_google_java_formatter():
 
     return os.path.abspath(gjf_jar)
 
-def jep396_args():
+def java_version(): 
     try:
         with subprocess.Popen("java -version", stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
             output = process.communicate()[1].decode("utf-8")
         if output.find('version "'):
-            majorVersion = output.split('"')[1].split(".")[0]
-            if int(majorVersion) >= 16:
-                return "--add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
+            major_version = output.split('"')[1].split(".")[0]
+            return int(major_version)
+    except ValueError:
+        return 1
+    return 1
+
+
+def jep396_args(major_version):
+    if major_version >= 16:
+        return "--add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
 --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
 --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
 --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
---add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
-        return ""
-    except ValueError:
-        return ""
-
+--add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED".split(" ")
+    return ""
+ 
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*',
@@ -54,13 +59,17 @@ def main(argv=None):
     args = parser.parse_args(argv)
 
     formatter = get_google_java_formatter()
-    jep_arg = jep396_args()
+    java_version = java_version()
+    if java_version < 11:
+        print("ERROR: Java v11 or greater is required to run the google-java-formatter. Will exit.")
+        return 1
+    jep_arg = jep396_args(java_version)
     if not jep_arg: 
         return subprocess.call([
             'java', '-jar', formatter, '--replace'] + args.filenames)
     else:
         return subprocess.call([
-            'java', jep_arg, '-jar', formatter, '--replace'] + args.filenames)
+            'java'] + jep_arg + ['-jar', formatter, '--replace'] + args.filenames)
 
 
 if __name__ == "__main__":
